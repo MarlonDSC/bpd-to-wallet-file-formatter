8584d0ff7862bf5c4a4be92a16f1708db5d05855
MarlonDSC <marlonsubuyu.2012@gmail.com>
2026-01-25 15:21:33 -0400

    🚨 #3 update this function so that its implementation is not identical to the one on line 14
    https://sonarcloud.io/project/issues?pullRequest=14&open=AZvy_hK1ofE685It17D4&id=CodeFlow-Dynamics_bpd-to-wallet-file-formatter

--- src/application/files/mappers/FileMapper.ts
+++ src/application/files/mappers/FileMapper.ts
@@ -7,40 +7,30 @@ import type { UploadedFileDto } from '../dtos/UploadedFileDto';
 import type { FileUploadViewModel } from '../../../presentation/converter/view-models/FileUploadViewModel';
 
 export class FileMapper {
+  private static fromDomainToDto(entity: UploadedFile): UploadedFileDto {
+    return {
+      id: entity.id,
+      name: entity.name,
+      size: entity.size.getBytes(),
+      sizeInMB: entity.size.toMB(),
+      type: entity.type.getExtension(),
+      file: entity.file,
+    };
+  }
+
   static toDomain(file: File, id?: string): UploadedFile {
     return UploadedFile.fromFile(file, id);
   }
 
   static toDto(entity: UploadedFile): UploadedFileDto {
-    return {
-      id: entity.id,
-      name: entity.name,
-      size: entity.size.getBytes(),
-      sizeInMB: entity.size.toMB(),
-      type: entity.type.getExtension(),
-      file: entity.file,
-    };
+    return FileMapper.fromDomainToDto(entity);
   }
 
   static toViewModel(entity: UploadedFile): FileUploadViewModel {
-    return {
-      id: entity.id,
-      name: entity.name,
-      size: entity.size.getBytes(),
-      sizeInMB: entity.size.toMB(),
-      type: entity.type.getExtension(),
-      file: entity.file,
-    };
+    return FileMapper.toViewModelFromDto(FileMapper.toDto(entity));
   }
 
   static toViewModelFromDto(dto: UploadedFileDto): FileUploadViewModel {
-    return {
-      id: dto.id,
-      name: dto.name,
-      size: dto.size,
-      sizeInMB: dto.sizeInMB,
-      type: dto.type,
-      file: dto.file,
-    };
+    return dto;
   }
 }
